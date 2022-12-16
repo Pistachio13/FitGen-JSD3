225c7e053c46eb48192ffb2dbbc82caa12a40d61
nabhatra <108543376+nabhatra@users.noreply.github.com>
2022-12-17 05:47:37 +0700

    Delete components directory

--- components/Context/GraphContextProvider.jsx
+++
@@ -1,8 +0,0 @@
-import GraphContext from "./GraphContext"
-import { useState } from "react"
-
-export default function GraphContextProvider ({children}) {
-    const [number, setNumber] = useState(0)
-    const [shouldShowPopup,setShouldShowPopup] = useState(false)
-    return (<GraphContext.Provider value = {{number,setNumber,shouldShowPopup,setShouldShowPopup}}> {children} </GraphContext.Provider>)
-}
